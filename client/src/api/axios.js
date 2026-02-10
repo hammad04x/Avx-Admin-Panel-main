fc86995dedb500b0bca7121c59a56b42bb5b0be4
Jagarala Hammad Imran <jagralahammadimran00@gmail.com>
2026-02-10 12:51:47 +0530

    Update API base URL in axios configuration

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.0.188:8104/api",
+  baseURL: "http://192.168.0.138:8104/api",
   // baseURL: "http://10.201.29.2:8104/api",
   headers: {
     "Content-Type": "application/json",
